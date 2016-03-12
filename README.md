68fac39ce1aa16ab43388d543b8213aebc6ad0fe
Denis Apostol <Denis Apostol>
2016-03-12 18:50:52 +0200

    Remont Ish

---
+++ README.md
@@ -0,0 +1,5 @@
+# http://postolenko.github.io/remontish/index.html
+# http://postolenko.github.io/remontish/remont_komputerov.html
+# http://postolenko.github.io/remontish/all_testimonials.html
+# http://postolenko.github.io/remontish/put_testimonial.html
+# http://postolenko.github.io/remontish/order.html
---
+++ js/scripts.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+
+
+    var questionIndex = 0;
+    var questionIndexActive;
+    var triangleHeight;
+
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+    getScrollToTopBtn();
+
+    showAnswerContent();
+
+    getHeightTriangle();
+
+    $(document).scroll(function() {
+
+        getScrollToTopBtn();
+
+    });
+
+    $(window).resize(function() {
+
+        getHeightTriangle();
+
+    });
+
+    $(function() {
+
+        $(".question").click( function(e) {
+
+            e.preventDefault();
+
+            if( !$(this).hasClass("active") ) {
+
+                $(".question").removeClass("active");
+
+                $(this).addClass("active");
+
+                showAnswerContent();
+
+            }          
+
+        } );
+
+    });
+
+    function showAnswerContent() {
+
+        $(".answer").css({"display":"none"});
+
+        for ( questionIndex = 0;  questionIndex <= $(".questions-list li").length; ++questionIndex ) {
+
+            if( $(".questions-list .question:eq("+ questionIndex +")").hasClass("active") ) {
+
+                $(".answers-box .answer:eq("+ questionIndex +")").fadeIn(300);
+
+            }
+
+        }
+
+    }
+
+
+    function getHeightTriangle() {
+
+        for ( questionImdex = 0;  questionImdex <= $(".questions-list li").length; ++questionImdex) {
+
+            triangleHeight = $(".questions-list .question:eq("+ questionImdex +")").outerHeight(true) / 2;
+
+            $(".questions-list .right-trg:eq("+ questionImdex +")").css({
+
+                                    "border-top": triangleHeight + "px solid transparent",
+                                    "border-bottom": triangleHeight + "px solid transparent"
+
+                                });
+
+        }
+
+    }
+
+
+    // Show or hide ".scroll-to-top" button 
+    function getScrollToTopBtn() {
+
+        if ($(window).scrollTop() > $('.head-section').height() ) {
+
+            $('.scroll-to-top').fadeIn();
+
+        } else {
+
+            $('.scroll-to-top').fadeOut();
+
+        }
+
+    }
+
+
+    $('.scroll-to-top').click(function () {
+
+        $('body,html').animate({
+
+            scrollTop: 0
+
+        }, 1000);
+
+        return false;
+
+    });    
+
+
+});
